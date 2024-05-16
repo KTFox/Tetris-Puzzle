2b52c30c54964e047ff424b57e0b0bf2f8e5d80b
Tran Xuan Khoi <khoitran7122002@gmail.com>
2024-05-16 15:51:28 +0700

    Add DrawEmptyCells method into Board script

--- Tetris_UnityBuild/Assets/Scripts/Core/Board.cs
+++ Tetris_UnityBuild/Assets/Scripts/Core/Board.cs
@@ -4,6 +4,31 @@ namespace TetrisPuzzle.Core
 {
     public class Board : MonoBehaviour
     {
-        
+        // Variables
+
+        [SerializeField] private Transform emptySquare;
+        [SerializeField] private Vector2Int size = new Vector2Int(10, 30);
+        [SerializeField] private int header = 8;
+
+
+        // Methods
+
+        private void Start()
+        {
+            DrawEmptyCells();
+        }
+
+        private void DrawEmptyCells()
+        {
+            for (int i = 0; i < size.y - header; i++)
+            {
+                for (int j = 0; j < size.x; j++)
+                {
+                    Transform cell = Instantiate(emptySquare, new Vector3(j, i, 0), Quaternion.identity);
+                    cell.parent = transform;
+                    cell.name = $"Board space ({j}, {i})";
+                }
+            }
+        }
     }
 }
